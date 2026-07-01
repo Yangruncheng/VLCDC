64599a538049d844a2809e76338e8ceae28afa31
啥也不会 <85560649+Yangruncheng@users.noreply.github.com>
2026-07-01 16:43:32 +0800

    Remove UDA-style run script from market_uda.sh

--- cluster-contrast-reid/market_uda.sh
+++ cluster-contrast-reid/market_uda.sh
@@ -1,10 +1,1 @@
-# Optional UDA-style run after preparing a source-domain checkpoint.
-CUDA_VISIBLE_DEVICES=0,1 python examples/cluster_contrast_train_usl.py \
-  --dataset market1501 \
-  --batch-size 256 \
-  --iters 200 \
-  --eps 0.6 \
-  --self-norm \
-  --use-hard \
-  --num-instances 8 \
-  --logs-dir logs/msmt17_to_market1501_sg
+empty
